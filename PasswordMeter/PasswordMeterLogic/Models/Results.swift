60727b8ef13f00e504594ec1fccb10c81b08e8d8
Pedro Neto <pedro.santosnt@hotmail.com>
2021-05-18 12:37:05 -0300

    add Results struct, for returning values

---
+++ PasswordMeter/PasswordMeterLogic/Models/Results.swift
@@ -0,0 +1,15 @@
+//
+//  Result.swift
+//  PasswordMeter
+//
+//  Created by Pedro Neto on 16/05/21.
+//
+
+import Foundation
+
+struct Results {
+    var className: String
+    var count: Int
+    var bonus: Int
+    var requirementLevel: RequirementLevel
+}
