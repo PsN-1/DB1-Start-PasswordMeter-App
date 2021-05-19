ba7903769c9201e0c994dccee9154b353b8e35d2
Pedro Neto <pedro.santosnt@hotmail.com>
2021-05-18 22:56:27 -0300

    refactor NumbersOnlyTest functions names to begin with "test"

--- PasswordMeterTests/DeductionsTests/NumbersOnlyTest.swift
+++ PasswordMeterTests/DeductionsTests/NumbersOnlyTest.swift
@@ -9,15 +9,15 @@ import XCTest
 @testable import PasswordMeter
 
 class NumbersOnlyTest: XCTestCase {
-    
-    func shouldReturnZeroNumbersOnlyWithRequirementSufficient() {
+
+    func testShouldReturnZeroNumbersOnlyWithRequirementSufficient() {
         let result = NumbersOnly("asdasS").getResult()
         XCTAssertEqual(0, result.count)
         XCTAssertEqual(0, result.bonus)
         XCTAssertEqual(RequirementLevel.SUFFICIENT, result.requirementLevel)
     }
 
-    func shouldReturnDeductionNumberOnlyWithRequirementWarning() {
+    func testShouldReturnDeductionNumberOnlyWithRequirementWarning() {
         let result = NumbersOnly("123456").getResult()
         XCTAssertEqual(6, result.count)
         XCTAssertEqual(6, result.bonus)
